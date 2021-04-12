40eeee63bc5f20dd4757694173bde45d22887c2b
JoseUtrilla-MBA <utrilla9@gmail.com>
2021-04-12 09:39:02 +0200

    Restore Interface AnagramsInterface

--- src/main/scala/week6/Anagrams.scala
+++ src/main/scala/week6/Anagrams.scala
@@ -165,7 +165,7 @@ object Anagrams extends AnagramsInterface {
 object Dictionary {
   def loadDictionary: List[String] = {
     val wordstream = Option {
-      getClass.getResourceAsStream(List("forcomp", "linuxwords.txt").mkString( "/", "/", ""))
+      getClass.getResourceAsStream(List("forcomp", "linuxwords.txt").mkString("/", "/", ""))
     } getOrElse {
       sys.error("Could not load word list, dictionary file not found")
     }
